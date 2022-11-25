8275cb7d27bf2da87d1881d7aff487cc1c81a5a1
Salomon Chambi <salomonchambi@gmail.com>
2022-11-25 08:39:22 -0800

    fix: conditional syntax in `all` method

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-
-from sqlalchemy import (create_engine)
+""" This module defines a class to manage db storage for hbnb clone """
+from sqlalchemy import create_engine, MetaData
 from os import getenv
-from sqlalchemy import MetaData
 from logging import info
 
-from sqlalchemy.orm import scoped_session
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session, sessionmaker
 from models.user import User
 from models.place import Place
 from models.state import State
@@ -15,11 +13,12 @@ from models.amenity import Amenity
 from models.review import Review
 from models.base_model import Base
 
-sql = 'mysql+mysqldb://{}:{}@{}:3306/{}'
+sql = 'mysql+mysqldb://{}:{}@{}:3306/{}'  # dialect: mysql, driver: mysqldb
 user = getenv('HBNB_MYSQL_USER')
 pssw = getenv('HBNB_MYSQL_PWD')
 host = getenv('HBNB_MYSQL_HOST')
 db = getenv('HBNB_MYSQL_DB')
+
 
 class DBStorage:
     """ Manages storage in the database"""
@@ -28,7 +27,7 @@ class DBStorage:
     __session = None
 
     def __init__(self):
-        """ Creates a new instance """
+        """ Creates a new engine instance """
         # pool_pre_ping=True, Boolean, test liveness connections each checkout
         self.__engine = create_engine(sql.format(user, pssw, host, db),
                                       pool_pre_ping=True)
@@ -37,39 +36,19 @@ class DBStorage:
             Base.metadata.drop_all(bind=self.__engine, checkfirst=True)
 
     def all(self, cls=None):
-        """all objects"""
+        """ Queries a database for objects """
         dic = {}
         if cls:
-                objs = self.__session.query(cls).all()
-            for obj in objs:
-                key = type(obj).__name__ + "." + str(obj.id)
-                dic[key] = obj
+            # .all() method returns the result of the query as a list
+            query_list = self.__session.query(cls).all()
+            for elem in query_list:
+                key = "{}.{}".format(type(elem).__name__, elem.id)
+                dic[key] = elem
         else:
             lista = [State, City, User, Place, Review, Amenity]
             for clase in lista:
                 query = self.__session.query(clase)
                 for elem in query:
                     key = "{}.{}".format(type(elem).__name__, elem.id)
-                    dic[key] = elem        
+                    dic[key] = elem
         return dic
-
-
-
-
-
-
- 
-        dic = {}
-        if cls is None:
-            objs = []
-            classes = ['User', 'State', 'City', 'Place', 'Review', 'Amenity']
-            for _class in classes:
-                results = self.__session.query(eval(_class))
-                for result in results:
-                    objs.append(result)
-        else:
-            objs = self.__session.query(cls).all()
-        for obj in objs:
-            key = type(obj).__name__ + "." + str(obj.id)
-            dic[key] = obj
-        return dic     
