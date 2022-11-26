a3f5dc6e10c3df584c6ac7be163e8eabf53eeba5
JPTicse <jpticse@gmail.com>
2022-11-25 22:15:32 -0500

    feat

--- models/city.py
+++ models/city.py
@@ -6,14 +6,13 @@ from sqlalchemy.sql.sqltypes import String
 from sqlalchemy.orm import relationship, backref
 from os import getenv
 
+
 if getenv('HBNB_TYPE_STORAGE') == 'db':
     class City(BaseModel, Base):
         """ The city class, contains state ID and name """
         __tablename__ = 'cities'
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship('Place', backref='cities',
-                              cascade="all, delete, delete-orphan")
 else:
     class City(BaseModel):
         """ The city class, contains state ID and name """
--- models/state.py
+++ models/state.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.sql.schema import Column, ForeignKey
+from sqlalchemy.sql.sqltypes import String
+from sqlalchemy.orm import relationship
+from models.city import City
+import models
+from os import getenv
 
 
-class State(BaseModel):
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    class State(BaseModel, Base):
+        """ State class """
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state')
+else: # File Storage
+    class State(BaseModel):
     """ State class """
     name = ""
