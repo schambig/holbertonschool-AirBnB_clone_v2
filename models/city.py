1ed9a8f4382ddcc6f6b1c39d6ee0c271f5295471
JPTicse <jpticse@gmail.com>
2022-11-26 00:50:12 -0500

    feat: Add class attributes and relationships

--- models/city.py
+++ models/city.py
@@ -13,6 +13,8 @@ if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'cities'
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship('Place', backref='cities',
+                              cascade="all, delete, delete-orphan")
 else:
     class City(BaseModel):
         """ The city class, contains state ID and name """
--- models/user.py
+++ models/user.py
@@ -17,6 +17,8 @@ if getenv('HBNB_TYPE_STORAGE') == 'db':
         password = Column(String(128), nullable=False)
         first_name = Column(String(128))
         last_name = Column(String(128))
+        places = relationship("Place", backref="user",
+                              cascade="all, delete, delete-orphan")
 else:
     class User(BaseModel):
         """This class defines a user by various attributes"""
