38699ba1f4f738aa09b5444b51c6edeee723b793
Salomon Chambi <salomonchambi@gmail.com>
2022-12-02 00:27:17 -0500

    fix: correct pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,10 +3,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def greeting():
     ''' Display a message to the user '''
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
